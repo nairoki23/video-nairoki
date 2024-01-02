f2c3876614e30158a12b4b14894520be7b449a4b
nairoki <nairoki.dev@gmail.com>
2024-01-02 13:47:28 +0900

    初期セット

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import { Box, Text } from "@kuma-ui/core";
+export default function Home() {
+
+
+
+  return (
+    <main>
+        <Box >
+            <Text fontSize={24} fontWeight={"bold"}>
+                nairoki動画
+            </Text>
+        </Box>
+    </main>
+  )
+}
