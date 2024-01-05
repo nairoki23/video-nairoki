b7df29aeae5d06a5d114b3dec799db1f39a474af
nairoki <nairoki.dev@gmail.com>
2024-01-05 11:28:39 +0900

    not-foundを整備

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+import {Button, Heading, VStack} from "@kuma-ui/core";
+import Link from "next/link";
+export default function NotFound() {
+  return (
+      <VStack justify="center" alignItems="center" width={"80%"} paddingTop={"5%"}>
+        <Heading as={"h1"}>ページが見つかりませんでした。</Heading>
+        <Link href={"/"} ><Button fontSize={"24px"}>ホームに戻る</Button></Link>
+      </VStack>
+  )
+}
